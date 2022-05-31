5914b3608eb64d8f5d05a4462f666cae9c920ff5
Cal Crosby <calcruiseby@gmail.com>
2022-05-31 01:53:51 -0700

    Update glizzus_install.sh

--- glizzus_install.sh
+++ glizzus_install.sh
@@ -14,22 +14,31 @@ LIST_OF_APPS="neovim
 sudo apt install -y $LIST_OF_APPS
 wait $!
 
-# Installs JetBrains toolbox ----------------------------
 
-mkdir -p $HOME/.jetbrains
-python3 - $HOME << END
+# Creates a temporary web installer script ------------------------------------
+
+touch $HOME/tempwebinstaller.py
+echo \
+"
+#!/usr/bin/python
 from urllib.request import urlopen
 import sys
-url = 'https://download.jetbrains.com/toolbox/' \
-      'jetbrains-toolbox-1.24.12080.tar.gz'
+url, dir = sys.argv[1], sys.argv[2]
+print(sys.argv)
 opened_url = urlopen(url)
-print('Connected to JetBrains URL')
-user_home = sys.argv[1]
-directory = user_home + '/.jetbrains/toolbox.tar.gz'
-with open(directory, 'wb+') as output:
-    output.write(opened_url.read())
-print('JetBrains toolbox tar downloaded')
-END
+with open(dir, 'wb+') as output:
+    output.write(opened_url.read())" \
+        >> $HOME/tempwebinstaller.py
+
+
+# Installs JetBrains toolbox --------------------------------------------------
+
+mkdir -p $HOME/.jetbrains
+
+jet_url="https://download.jetbrains.com/toolbox/"\
+"jetbrains-toolbox-1.24.12080.tar.gz"
+
+python3 $HOME/tempwebinstaller.py $jet_url $HOME/.jetbrains/toolbox.tar.gz
 
 echo "Extracting tar"
 tar -xzf $HOME/.jetbrains/toolbox.tar.gz -C $HOME/.jetbrains
@@ -38,55 +47,99 @@ mv $HOME/.jetbrains/*/* $HOME/.jetbrains
 rm -r $HOME/.jetbrains/*/
 echo "JetBrains toolbox installed"
 
+
+# Change background -----------------------------------------------------------
+
+img_url="https://i.pinimg.com/originals/1f/"\
+"a3/2c/1fa32c8411034fc4e838bd83fb6c0fc7.jpg"
+
+python3 $HOME/tempwebinstaller.py $img_url $HOME/Pictures/background.jpg
+gsettings set org.gnome.desktop.background picture-uri \
+    file:////$HOME/Pictures/background.jpg
+
+
 # Configures neovim -------------------------------------
 
 echo "Configuring Neovim"
 
 mkdir -p $HOME/.config/nvim
 touch $HOME/.config/nvim/init.vim
-echo "set nocompatible
-set showmatch
+echo \
+"
+set nocompatible
+filetype plugin indent on
+
+set so=7
+set ruler
+set cmdheight=1
+set hid
+set wildmenu
 set ignorecase
-set mouse=v
+set smartcase
 set hlsearch
 set incsearch
-set tabstop=4
-set softtabstop=4
+set lazyredraw
+set magic
+set showmatch
+set mat=2
+
+syntax enable
+
+colorscheme industry
+
+set nobackup
+set nowb
+set noswapfile
+
+set expandtab
+set smarttab
 set shiftwidth=4
-set autoindent
+set tabstop=4
+
+set lbr
+set tw=500
+
+set ai
+set si
+set wrap
+
 set number
-set wildmode=longest,list
 set cc=80
-filetype plugin indent on
-syntax on
 set mouse=a
 set clipboard=unnamedplus
-set ttyfast
-set filetype
-filetype plugin on" \
+set ttyfast" \
 	>> $HOME/.config/nvim/init.vim
 
 echo "Adding file type plugins"
 
 mkdir -p $HOME/.config/nvim/ftplugin
 
-FILE_TYPE_PLUGINS=("python" "c" "cpp")
-for plugin in ${FILE_TYPE_PLUGINS[@]}; do
-	touch $HOME/.config/nvim/ftplugin/$plugin.vim
-	echo "config file for $plugin created"
+FILE_TYPES=("python" "c" "cpp")
+for file_type in ${FILE_TYPES[@]}; do
+	touch $HOME/.config/nvim/ftplugin/$file_type.vim
+	echo "config file for $file_type created"
 done
 
-echo "setlocal expandtab
+echo \
+"setlocal expandtab
 setlocal formatoptions=croql
 nnoremap <buffer> <F8> :w <CR> :!python3 % <CR>" \
 	>> $HOME/.config/nvim/ftplugin/python.vim
+
 
 # Configures bash aliases -----------------------------------------
 
 echo "configuring bash aliases"
 touch $HOME/.bash_aliases
 
-echo "alias editvim='vim ~/.config/nvim/init.vim" \
-	>> $HOME/.bash_aliases
+echo \
+" 
+alias editvim='vim ~/.config/nvim/init.vim'
+alias makevenv='python3 -m env ./venv'
+alias startvenv='. ./venv/bin/activate'" \
+    >> $HOME/.bash_aliases
+
+sudo apt autoremove
+rm $HOME/tempwebinstaller.py
 
 echo "done"
