338c7284a2f38845869469b7853eefeb8307107f
Cal Crosby <calcruiseby@gmail.com>
2022-06-01 01:31:34 -0700

    Update glizzus_install.sh

--- glizzus_install.sh
+++ glizzus_install.sh
@@ -1,145 +1,167 @@
 #!/bin/sh
 
 
-# Installs applications ----------------------------------
+install_applications() {
+    
+    LIST_OF_APPS="neovim
+                  python3
+                  python3-pip
+                  tmux
+                  tree
+                  lynx
+                  git
+                  curl
+                  openjdk-17-jdk-headless
+                  nodejs
+                  scala"
 
-LIST_OF_APPS="neovim 
-			  python3
-			  python3-pip
-			  tmux
-			  tree
-			  lynx
-			  git"
-
-sudo apt install -y $LIST_OF_APPS
-wait $!
-
-
-# Creates a temporary web installer script ------------------------------------
-
-touch $HOME/tempwebinstaller.py
-echo \
-"
-#!/usr/bin/python
-from urllib.request import urlopen
-import sys
-url, dir = sys.argv[1], sys.argv[2]
-print(sys.argv)
-opened_url = urlopen(url)
-with open(dir, 'wb+') as output:
-    output.write(opened_url.read())" \
-        >> $HOME/tempwebinstaller.py
+    sudo apt install -y $LIST_OF_APPS
+    wait $!
+}
 
 
-# Installs JetBrains toolbox --------------------------------------------------
+get_jetbrains_toolbox() {
+    
+    mkdir -p $HOME/.jetbrains
 
-mkdir -p $HOME/.jetbrains
-
-jet_url="https://download.jetbrains.com/toolbox/"\
+    jet_url="https://download.jetbrains.com/toolbox/"\
 "jetbrains-toolbox-1.24.12080.tar.gz"
 
-python3 $HOME/tempwebinstaller.py $jet_url $HOME/.jetbrains/toolbox.tar.gz
-
-echo "Extracting tar"
-tar -xzf $HOME/.jetbrains/toolbox.tar.gz -C $HOME/.jetbrains
-rm $HOME/.jetbrains/toolbox.tar.gz
-mv $HOME/.jetbrains/*/* $HOME/.jetbrains
-rm -r $HOME/.jetbrains/*/
-echo "JetBrains toolbox installed"
+    echo "Installing JetBrains Toolbox"
+    curl -L $jet_url | tar zx -C $HOME/.jetbrains --strip-components 1
+    echo "JetBrains toolbox installed"
+}
 
 
-# Change background -----------------------------------------------------------
-
-img_url="https://i.pinimg.com/originals/1f/"\
+change_background() {
+    
+    img_url="https://i.pinimg.com/originals/1f/"\
 "a3/2c/1fa32c8411034fc4e838bd83fb6c0fc7.jpg"
 
-python3 $HOME/tempwebinstaller.py $img_url $HOME/Pictures/background.jpg
-gsettings set org.gnome.desktop.background picture-uri \
-    file:////$HOME/Pictures/background.jpg
+    curl -o $HOME/Pictures/background.jpg $url
+    gsettings set org.gnome.desktop.background picture-uri \
+        file:////$HOME/Pictures/background.jpg
+}
 
 
-# Configures neovim -------------------------------------
+configure_neovim() {
 
-echo "Configuring Neovim"
+    echo "Configuring Neovim"
 
-mkdir -p $HOME/.config/nvim
-touch $HOME/.config/nvim/init.vim
-echo \
-"
-set nocompatible
-filetype plugin indent on
+    mkdir -p $HOME/.config/nvim
+    touch $HOME/.config/nvim/init.vim
+    echo \
+    "set nocompatible
+    filetype plugin indent on
 
-set so=7
-set ruler
-set cmdheight=1
-set hid
-set wildmenu
-set ignorecase
-set smartcase
-set hlsearch
-set incsearch
-set lazyredraw
-set magic
-set showmatch
-set mat=2
+    set so=7
+    set ruler
+    set cmdheight=1
+    set hid
+    set wildmenu
+    set ignorecase
+    set smartcase
+    set hlsearch
+    set incsearch
+    set lazyredraw
+    set magic
+    set showmatch
+    set mat=2
 
-syntax enable
+    syntax enable
 
-colorscheme industry
+    colorscheme industry
 
-set nobackup
-set nowb
-set noswapfile
+    set nobackup
+    set nowb
+    set noswapfile
 
-set expandtab
-set smarttab
-set shiftwidth=4
-set tabstop=4
+    set expandtab
+    set smarttab
+    set shiftwidth=4
+    set tabstop=4
 
-set lbr
-set tw=500
+    set lbr
+    set tw=500
 
-set ai
-set si
-set wrap
+    set ai
+    set si
+    set wrap
 
-set number
-set cc=80
-set mouse=a
-set clipboard=unnamedplus
-set ttyfast" \
-	>> $HOME/.config/nvim/init.vim
+    set number
+    set cc=80
+    set mouse=a
+    set clipboard=unnamedplus
+    set ttyfast" \
+        >> $HOME/.config/nvim/init.vim
 
-echo "Adding file type plugins"
+    echo "Adding file type plugins"
 
-mkdir -p $HOME/.config/nvim/ftplugin
+    mkdir -p $HOME/.config/nvim/ftplugin
 
-FILE_TYPES=("python" "c" "cpp")
-for file_type in ${FILE_TYPES[@]}; do
-	touch $HOME/.config/nvim/ftplugin/$file_type.vim
-	echo "config file for $file_type created"
-done
+    FILE_TYPES=("python" "c" "cpp")
+    for file_type in ${FILE_TYPES[@]}; do
+        touch $HOME/.config/nvim/ftplugin/$file_type.vim
+        echo "config file for $file_type created"
+    done
 
-echo \
-"setlocal expandtab
-setlocal formatoptions=croql
-nnoremap <buffer> <F8> :w <CR> :!python3 % <CR>" \
-	>> $HOME/.config/nvim/ftplugin/python.vim
+    echo \
+    "setlocal expandtab
+    setlocal formatoptions=croql
+    nnoremap <buffer> <F8> :w <CR> :!python3 % <CR>" \
+        >> $HOME/.config/nvim/ftplugin/python.vim
+
+}
 
 
-# Configures bash aliases -----------------------------------------
+configure_tmux() {
+    
+    touch $HOME/.tmux.conf
+    echo "Configuring tmux"
 
-echo "configuring bash aliases"
-touch $HOME/.bash_aliases
+    echo \
+    "unbind-key C-b
+    set-option -g prefix C-a
+    bind-key C-a send-prefix
 
-echo \
-" 
-alias editvim='vim ~/.config/nvim/init.vim'
-alias makevenv='python3 -m env ./venv'
-alias startvenv='. ./venv/bin/activate'" \
-    >> $HOME/.bash_aliases
+    set -g default-terminal screen-256color
 
-sudo apt autoremove
-rm $HOME/tempwebinstaller.py
+    set-window-option -g pane-border-style fg=colour11,bg=colour234
+    set-window-option -g pane-active-border-style fg=colour118,bg=colour234
 
-echo "done"
+    set-window-option -g window-style fg=white,bg=colour236
+    set-window-option -g window-active-style fg=white,bg=colour235" \
+        >> $HOME/.tmux.conf
+
+    echo "Tmux configured"
+}
+
+
+configure_aliases() {
+
+    echo "configuring bash aliases"
+    touch $HOME/.bash_aliases
+
+    echo \
+    "alias editvim='vim ~/.config/nvim/init.vim'
+    alias makevenv='python3 -m env ./venv'
+    alias startvenv='. ./venv/bin/activate'
+    alias toolbox='~/.jetbrains/./jetbrains-toolbox" \
+        >> $HOME/.bash_aliases
+}
+
+
+main() {
+    install_applications
+    get_jetbrains_toolbox
+    change_background
+    configure_neovim
+    configure_tmux
+    configure_aliases
+
+    sudo apt autoremove
+
+    echo "done"
+}
+
+main
