85279c5dc20fdcce0ac7c5e0b9063edac22b2cdd
Cal Crosby <calcruiseby@gmail.com>
2022-06-01 04:25:14 -0500

    Update glizzus_install.sh

--- glizzus_install.sh
+++ glizzus_install.sh
@@ -38,7 +38,7 @@ change_background() {
     img_url="https://i.pinimg.com/originals/1f/"\
 "a3/2c/1fa32c8411034fc4e838bd83fb6c0fc7.jpg"
 
-    curl -o $HOME/Pictures/background.jpg $url
+    curl -o $HOME/Pictures/background.jpg $img_url
     gsettings set org.gnome.desktop.background picture-uri \
         file:////$HOME/Pictures/background.jpg
 }
@@ -51,49 +51,41 @@ configure_neovim() {
     mkdir -p $HOME/.config/nvim
     touch $HOME/.config/nvim/init.vim
     echo \
-    "set nocompatible
-    filetype plugin indent on
-
-    set so=7
-    set ruler
-    set cmdheight=1
-    set hid
-    set wildmenu
-    set ignorecase
-    set smartcase
-    set hlsearch
-    set incsearch
-    set lazyredraw
-    set magic
-    set showmatch
-    set mat=2
-
-    syntax enable
-
-    colorscheme industry
-
-    set nobackup
-    set nowb
-    set noswapfile
-
-    set expandtab
-    set smarttab
-    set shiftwidth=4
-    set tabstop=4
-
-    set lbr
-    set tw=500
-
-    set ai
-    set si
-    set wrap
-
-    set number
-    set cc=80
-    set mouse=a
-    set clipboard=unnamedplus
-    set ttyfast" \
-        >> $HOME/.config/nvim/init.vim
+"set nocompatible
+filetype plugin indent on
+set so=7
+set ruler
+set cmdheight=1
+set hid
+set wildmenu
+set ignorecase
+set smartcase
+set hlsearch
+set incsearch
+set lazyredraw
+set magic
+set showmatch
+set mat=2
+syntax enable
+colorscheme industry
+set nobackup
+set nowb
+set noswapfile
+set expandtab
+set smarttab
+set shiftwidth=4
+set tabstop=4
+set lbr
+set tw=500
+set ai
+set si
+set wrap
+set number
+set cc=80
+set mouse=a
+set clipboard=unnamedplus
+set ttyfast" \
+    >> $HOME/.config/nvim/init.vim
 
     echo "Adding file type plugins"
 
@@ -106,9 +98,9 @@ configure_neovim() {
     done
 
     echo \
-    "setlocal expandtab
-    setlocal formatoptions=croql
-    nnoremap <buffer> <F8> :w <CR> :!python3 % <CR>" \
+"setlocal expandtab
+setlocal formatoptions=croql
+nnoremap <buffer> <F8> :w <CR> :!python3 % <CR>" \
         >> $HOME/.config/nvim/ftplugin/python.vim
 
 }
@@ -120,18 +112,17 @@ configure_tmux() {
     echo "Configuring tmux"
 
     echo \
-    "unbind-key C-b
-    set-option -g prefix C-a
-    bind-key C-a send-prefix
+"unbind-key C-b
+set-option -g prefix C-a
+bind-key C-a send-prefix
 
-    set -g default-terminal screen-256color
+set -g default-terminal screen-256color
 
-    set-window-option -g pane-border-style fg=colour11,bg=colour234
-    set-window-option -g pane-active-border-style fg=colour118,bg=colour234
-
-    set-window-option -g window-style fg=white,bg=colour236
-    set-window-option -g window-active-style fg=white,bg=colour235" \
-        >> $HOME/.tmux.conf
+set-window-option -g pane-border-style fg=colour11,bg=colour234
+set-window-option -g pane-active-border-style fg=colour118,bg=colour234
+set-window-option -g window-style fg=white,bg=colour236
+set-window-option -g window-active-style fg=white,bg=colour235" \
+    >> $HOME/.tmux.conf
 
     echo "Tmux configured"
 }
@@ -143,10 +134,10 @@ configure_aliases() {
     touch $HOME/.bash_aliases
 
     echo \
-    "alias editvim='vim ~/.config/nvim/init.vim'
-    alias makevenv='python3 -m env ./venv'
-    alias startvenv='. ./venv/bin/activate'
-    alias toolbox='~/.jetbrains/./jetbrains-toolbox" \
+"alias editvim='vim ~/.config/nvim/init.vim'
+alias makevenv='python3 -m env ./venv'
+alias startvenv='. ./venv/bin/activate'
+alias toolbox='~/.jetbrains/./jetbrains-toolbox" \
         >> $HOME/.bash_aliases
 }
 
